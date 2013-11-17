c97cf4a8eade5df8623dc6e42aefea68715515af
Ryan <rszemplinski22@gmail.com>
2013-11-17 13:55:04 -0500

    Some User stuffz

--- Gemfile
+++ Gemfile
@@ -16,6 +16,11 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 
+gem 'faker', '1.1.2'
+
+gem 'will_paginate', '3.0.4'
+gem 'will_paginate-bootstrap'
+
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
   
   def new
@@ -24,21 +28,50 @@ class UsersController < ApplicationController
   end
   
   def edit
-    
+    @user = User.find(params[:id])
   end
   
   def update
-    
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params) then
+      flash[:success] = "Successfully updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   def destroy
-    
+    @user = User.find(params[:id])
+    if !current_user?(@user) then
+      @user.destroy
+      flash[:success] = "User deleted!"
+      redirect_to users_path
+    else
+      flash[:danger] = "You tried to delete yourself you idiot!"
+      redirect_to users_path
+    end
   end
   
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation )
+    end
+    
+    def admin_user
+      redirect_to root_path unless current_user.admin?
+    end
+    
+    def signed_in_user
+      unless signed_in?
+        redirect_to login_path, notice: "Please sign in."
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
     end
   
 end
