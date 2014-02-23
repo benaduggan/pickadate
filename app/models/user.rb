4cb815c55a04d561c5618df6f2a7247d45e3db7d
Ben Duggan <benaduggan@gmail.com>
2014-02-23 04:37:58 +0000

    here is everything

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,14 @@
 class User < ActiveRecord::Base
   
-
-  
-    before_save { self.email = email.downcase }
+    #before_save { self.email = email.downcase }
     before_create :create_remember_token
     
     VALID_USERNAME_REGEX = /[a-z0-9_-]\z/
-    validates :username, presence: true, uniqueness: true, length: { maximum: 16 }, :format => { :with => VALID_USERNAME_REGEX }
+    validates :username, presence: true, uniqueness: true, length: { maximum: 30 }, :format => { :with => VALID_USERNAME_REGEX }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
     validates :password, length: { minimum: 6}
     has_secure_password
-  
   
     def self.from_omniauth(auth)
       if auth #hash that comes back from Facebook, this is so we can do both facebook and normal users through same sessions controller
@@ -19,8 +16,10 @@ class User < ActiveRecord::Base
           user.provider = auth.provider
           user.uid = auth.uid
           user.username = auth.info.name unless user.username?
+          
           user.password = User.generate_password
-          user.password_confirmation = user.password          
+          user.password_confirmation = user.password
+          
           user.firstname = auth.info.first_name unless user.firstname?
           user.lastname = auth.info.last_name unless user.lastname?
           user.email = auth.info.email unless user.email?
@@ -63,4 +62,5 @@ class User < ActiveRecord::Base
     def create_remember_token
       self.remember_token = User.encrypt(User.new_remember_token)
     end
+    
 end
