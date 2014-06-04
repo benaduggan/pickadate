0c7adc77e2a11aef05d2f7633b03ac6ea21a0b4b
Ben Duggan <benaduggan@gmail.com>
2014-06-04 00:01:02 +0000

    more pickadate stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140603003730) do
+ActiveRecord::Schema.define(version: 20140603235259) do
 
   create_table "pickadates", force: true do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140603003730) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "floor"
+    t.boolean  "owner"
   end
 
   create_table "users", force: true do |t|
